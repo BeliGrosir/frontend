ec2aec3ed170f681879affc4a13a811f04147586
Inez Amandha <inez.intern@mail.sirclo.com>
2021-11-20 17:22:27 +0700

    style: fix navigation bar style

--- components/NavigationBar/index.js
+++ components/NavigationBar/index.js
@@ -8,42 +8,40 @@ import Link from 'next/link'
 
 export default function NavigationBar() {
     return (
-        <div className={styles}>
-            <Navbar collapseOnSelect expand="lg" className={styles.navbar}>
-                <Container>
-                    <Navbar.Brand href="#home">
-                        <Image
-                            src={Logo}
-                            alt="BeliGrosir"
-                            width={500}>
-                        </Image>
-                    </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav>
-                            <Link href="/">
-                                <a className={`${styles.button} ${styles.signUpButton}`}>
-                                    Sign Up
-                                </a>
-                            </Link>
-                            <Link href="/">
-                                <a className={`${styles.button} ${styles.loginButton}`}>
-                                    Login
-                                </a>
-                            </Link>
-                            <Link href="/">
-                                <a className={styles.cart}>
-                                    <Image
-                                        src={Cart}
-                                        alt="Cart"
-                                        height={35}>
-                                    </Image>
-                                </a>
-                            </Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar >
-        </div >
+        <Navbar collapseOnSelect expand="md" fixed="top" className={styles.navbar}>
+            <Container>
+                <Navbar.Brand href="#home">
+                    <Image
+                        src={Logo}
+                        alt="BeliGrosir"
+                        height={35}>
+                    </Image>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="ms-auto">
+                        <Link href="/">
+                            <a className={`${styles.button} ${styles.signUpButton}`}>
+                                Sign Up
+                            </a>
+                        </Link>
+                        <Link href="/">
+                            <a className={`${styles.button} ${styles.loginButton}`}>
+                                Login
+                            </a>
+                        </Link>
+                        <Link href="/">
+                            <a className={styles.cart}>
+                                <Image
+                                    src={Cart}
+                                    alt="Cart"
+                                    height={35}>
+                                </Image>
+                            </a>
+                        </Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar >
     )
 }
