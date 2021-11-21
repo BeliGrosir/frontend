924e990386bec4687bc203fe82354fceea9e934a
ray_arwindra <wan.muhammad81@ui.ac.id>
2021-11-21 08:48:36 +0700

    Finished PDP

--- components/productCard.jsx
+++ components/productCard.jsx
@@ -1,18 +1,23 @@
-export default function ProductCard({image, title, retail, price, location  }) {
-    return (
-        <div>
-            <div>
-                {image}
-            </div>
-            <div>
-                {title}
-                Retail: Rp{retail}/kg
-                Rp{price}/kg
-                <button>Add to Cart</button>
-            </div>
-            <div>
-                {location}
-            </div>
-        </div>
-    )
+import { Col } from "react-bootstrap";
+import Image from "next/image";
+import styles from "../styles/components/ProductCard.module.css";
+export default function ProductCard({ image, title, retail, price, location }) {
+  return (
+    <Col className={styles.main} xs={3}>
+      <div className={styles.image}>
+        <Image src={image} layout="fill" />
+      </div>
+      <div>
+        <h1 className={styles.title}>{title}</h1>
+        <p className={styles.retail}>
+          Retail: <span>Rp{retail}/kg</span>
+        </p>
+        <p className={styles.price}>Rp{price}/kg</p>
+        <button className={styles.button}>Add to Cart</button>
+      </div>
+      <div className={styles.location}>
+        <p>{location}</p>
+      </div>
+    </Col>
+  );
 }
--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -1,6 +1,22 @@
 import Head from "next/head";
+import Navbar from "../../components/NavigationBar";
+import Almond from "../../public/deleteLater/almond.png";
+import Brand from "../../public/deleteLater/brand.png";
+import Image from "next/image";
+import { Row, Col, Container } from "react-bootstrap";
+import ProductCard from "../../components/productCard";
+import styles from "../../styles/pages/Product.module.css";
 
 export default function ProductDetail() {
+const productReccomendations = [
+    <ProductCard image={Almond} title={"Almonds"} retail={"75.000"} price={"65.000"} location={"Jakarta Selatan"} />,
+    <ProductCard image={Almond} title={"Almonds"} retail={"75.000"} price={"65.000"} location={"Jakarta Selatan"} />,
+    <ProductCard image={Almond} title={"Almonds"} retail={"75.000"} price={"65.000"} location={"Jakarta Selatan"} />,
+    <ProductCard image={Almond} title={"Almonds"} retail={"75.000"} price={"65.000"} location={"Jakarta Selatan"} />,
+    <ProductCard image={Almond} title={"Almonds"} retail={"75.000"} price={"65.000"} location={"Jakarta Selatan"} />,
+    <ProductCard image={Almond} title={"Almonds"} retail={"75.000"} price={"65.000"} location={"Jakarta Selatan"} />,
+    <ProductCard image={Almond} title={"Almonds"} retail={"75.000"} price={"65.000"} location={"Jakarta Selatan"} />,
+]
   return (
     <div>
       <Head>
@@ -11,31 +27,40 @@ export default function ProductDetail() {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <div>
-          <div></div>
-          <div>
-            <div>
-              <h3>Almonds</h3>
-              <p>Food Ingredients</p>
-            </div>
-            <div>
-              <p>
-                Retail: <span>Rp.75.000/kg</span>
-              </p>
-              <h3>Rp.65.000/kg</h3>
-            </div>
-            <button>Add to Cart</button>
-            <div></div>
-            <p>Naked Inc.</p>
-          </div>
-        </div>
-        <div>
+      <Navbar />
+      <main className={styles.main}>
+        <Container>
+          <Row>
+            <Col xs={4}>
+              <div className={styles.image}>
+                <Image src={Almond} layout="fill" />
+              </div>
+            </Col>
+            <Col xs={8}>
+              <div className={styles.title}>
+                <h3>Almonds</h3>
+                <p>Food Ingredients</p>
+              </div>
+              <div className={styles.price}>
+                <p>
+                  Retail: <span>Rp.75.000/kg</span>
+                </p>
+                <h3>Rp.65.000/kg</h3>
+              </div>
+              <button className={styles.addToCart}>Add to Cart</button>
+              <div className={styles.brand}>
+                <Image src={Brand} />
+                <p>Naked Inc.</p>
+              </div>
+            </Col>
+          </Row>
+          <div className={styles.simProducts}>
             <h4>Similar Products</h4>
-            <div>
-                
-            </div>
-        </div>
+            <Row>
+                {productReccomendations}
+            </Row>
+          </div>
+        </Container>
       </main>
     </div>
   );
